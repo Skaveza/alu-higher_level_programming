a231db5050d0df2979fe9d1255b64bd92bb42e62
Skaveza <s.mwachoni@alustudent.com>
2023-07-27 05:41:17 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-'''Define a class called Square that defines a square'''
-
+'''Defined a class called Square which represents a aquare'''
 
 
 class Square:
- '''Square class with private size attribute'''
-    def __init__(self,size):
-    '''Initializes instance of size'''
+    '''Square class with private size attr'''
 
+    def __init__(self, size):
+        '''Initializes instances of Square.
 
-    Args:
-        size (int): represents size of one of the squares.
+           Args:
+                size (int): Represents the size of one side of the square.
         '''
         self.__size = size
