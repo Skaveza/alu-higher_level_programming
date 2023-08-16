863d6604f399bfccc5716e1738a46a250e3bf7c8
Skaveza <s.mwachoni@alustudent.com>
2023-08-16 16:48:03 -0700

    square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,6 +4,7 @@ Write the class Square that inherits from Rectangle
 """
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """ This class defines the blueprint for a Square object """
     def __init__(self, size, x=0, y=0, id=None):
@@ -38,4 +39,23 @@ class Square(Rectangle):
                     self.height = value
                 else:
                     setattr(self, key, value)
+
+
+if __name__ == "__main__":
+    s1 = Square(5)
+    print(s1)
+    s1.update(10)
+    print(s1)
+    s1.update(1, 2)
+    print(s1)
+    s1.update(1, 2, 3)
+    print(s1)
+    s1.update(1, 2, 3, 4)
+    print(s1)
+    s1.update(x=12)
+    print(s1)
+    s1.update(size=7, y=1)
+    print(s1)
+    s1.update(size=7, id=89, y=1)
+    print(s1)
 
